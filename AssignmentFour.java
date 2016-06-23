0dd93592bed8cb324bb5c61afc30330c562d498e
Manmohan Shaw <shawmanmohan@gmail.com>
2016-06-23 19:49:24 +0530

    Session2:AssignmentFour

--- AssignmentFour.java
+++ AssignmentFour.java
@@ -1,20 +1,39 @@
-package com.sessionone;
+package com.sessiontwo;
 /*
- * Assume that the integer variable x is 5 and the integer variable y is 10.
- *  Give the values of the following expressions:
-a. x + y * 2
-b. x - y + 2
-c. (x+y) * 2
-d. y % x
+ * Write a program to print total number of days in the given month.
  */
+
 public class AssignmentFour {
-	public static void main(String args[])
-	{
-		int x=5,y=10;
-		System.out.println("Value of x + y * 2 is " + (x+y*2) );
-		System.out.println("Value of x - y + 2 is "+ (x-y+2));
-		System.out.println("Value of (x+y) * 2 is "+ ((x+y)*2));
-		System.out.println("Value of y % x is "+ y%x);
+	
+	public static void main(String[] args) {
+		String month=args[0];
+		switch(month){
+		case "January": System.out.println(month + " has 31 Days");
+		break;
+		case "Feburary": System.out.println(month + " has 28 Days");
+		break;
+		case "March": System.out.println(month + " has 30 Days");
+		break;
+		case "April": System.out.println(month + " has 31 Days");
+		break;
+		case "May": System.out.println(month + " has 30 Days");
+		break;
+		case "June": System.out.println(month + " has 31 Days");
+		break;
+		case "July": System.out.println(month + " has 30 Days");
+		break;
+		case "August": System.out.println(month + " has 31 Days");
+		break;
+		case "September": System.out.println(month + " has 30 Days");
+		break;
+		case "October": System.out.println(month + " has 31 Days");
+		break;
+		case "November": System.out.println(month + " has 30 Days");
+		break;
+		case "December": System.out.println(month + " has 31 Days");
+		break;
+		default : System.out.println("Enter a valid Month");
+		}
 	}
 
 }
